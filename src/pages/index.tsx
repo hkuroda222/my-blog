b9d16edf3d4d7354b87268b13ce63587faf15bdf
HirokiKuroda <kinaco.ao@gmail.com>
2023-02-24 00:41:58 +0900

    マークダウンファイルを取得できるように追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import fs from 'fs';
+import { NextPage } from 'next';
+import Link from 'next/link';
+import matter from 'gray-matter';
 
-export default function Home() {
+import Head from 'next/head';
+
+const Home: NextPage<{
+  posts: Array<{
+    frontMatter: { date: string; description: string; title: string };
+    slug: string;
+  }>;
+}> = (props) => {
+  const { posts } = props;
+  console.log(posts);
   return (
     <>
       <Head>
@@ -10,9 +21,35 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className="my-8">コンテンツ</div>
+      <main>
+        <div className="my-8">
+          {posts.map((post) => (
+            <div key={post.slug}>
+              <Link href={`posts/${post.slug}`}>{post.frontMatter.title}</Link>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
-}
+};
+
+export const getStaticProps = () => {
+  // posts内のファイルを配列で取得
+  const files = fs.readdirSync('./src/posts');
+
+  // ファイルの中身を取得
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace(/\.md$/, ''); // 末尾の.mdを削除
+    const fileContent = fs.readFileSync(`./src/posts/${fileName}`, 'utf-8');
+    const { data } = matter(fileContent); // front-matterを取得
+    return { frontMatter: data, slug: slug };
+  });
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+};
+
+export default Home;
---
+++ src/posts/sample.md
@@ -0,0 +1,13 @@
+---
+title: 'Next.jsでmarkdownブログを構築'
+date: '2023-02-23'
+description: 'サンプル用のmdファイル'
+---
+
+Next.js を使って Markdown のブログサイトの構築を一から行なっていきます。
+
+## h2 タイトル
+
+### h3 タイトル
+
+npx create-next-app コマンドを利用して Next.js プロジェクトの作成を行います。
